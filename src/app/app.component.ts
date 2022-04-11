ed4419f8b70c41adce35204415cd636b90ff11b5
HarshKaushik <kaushikharsh86@gmail.com>
2022-04-11 12:11:15 +0530

    form ui bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = [
+    'Web Development',
+    'Mobile App Development',
+    'Competitive Progrmaing',
+    'Data Structure & Algo',
+    'Machine Learning',
+    'Artificial Intelligence',
+  ];
 }
